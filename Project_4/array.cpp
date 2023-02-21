4edb24454de2ae9cee6d410974fe807d177dd8f1
HarrisDoan <hdoan01@g.ucla.edu>
2023-02-20 16:14:01 -0800

    Updates!! Let's GOOOO!
    Figured out the floating point counter by utilizing a pointer and built in string functions.

--- Project_4/array.cpp
+++ Project_4/array.cpp
@@ -11,6 +11,8 @@ int locateMaxium(const string array[], int n);
 int countFloatingPointValues(const string array[], int n);
 bool hasNoCapitals(const string array[], int n);
 int shiftLeft(string array[], int n, int amount, string placeholder);
+bool validInteger(string array);
+bool validFloat(string array);
 
 // Void function to sort array
 void sort_string(string array[], int n)
@@ -35,6 +37,67 @@ string returnMax(string array[], int n)
     sort_string(array, n);
     string search = array[n - 1];
     return search;
+}
+
+// Similar to Project 3 with validQC, I can repeat the same logic with a validInteger
+bool validInteger(string array)
+{
+    if (array.empty())
+    {
+        return false;
+    }
+
+    // for our case, a valid input cannot have a leading - or +
+    if (((!isdigit(array[0])) && (array[0] == '-') && (array[0] == '+')))
+    {
+        return false;
+    }
+
+    // Initialize a pointer and use strtol to convert string to long int.
+    char *p;
+    long check;
+    check = strtol(array.c_str(), &p, 10);
+
+    // if my pointer points to the end of the string, that means the entire string was converted properly converted to a integer,
+    // this means the entire string must be a valid integer. Therefore, if *p == 0, then I can return true;
+    // Ex: "12123asb" the pointer would not point to the end of the string since it stops in the middle at a since letters dont get converted.
+    // For this example: *p != 0. So I can return false.
+
+    if (*p == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+// Same idea as validInteger
+bool validFloat(string array)
+{
+    if (array.empty())
+    {
+        return false;
+    }
+
+    // for our case, a valid input cannot have a leading - or +
+    if (((!isdigit(array[0])) && (array[0] == '-') && (array[0] == '+')))
+    {
+        return false;
+    }
+
+    // Initialize a pointer and use strtod to convert string to a double (similar enought to a float).
+    char *p;
+    double check;
+    check = strtod(array.c_str(), &p);
+
+    // if my pointer points to the end of the string which means the entire string was converted to a double, then the entire string must
+    // be a valid float. Therefore, if *p is 0, then I can return true;
+    if (*p == 0)
+    {
+        return true;
+    }
+
+    return false;
 }
 
 // Create a copy of the array to sort using the created sort function and then search for the maximum, returns the index of the maximum
@@ -74,23 +137,21 @@ int countFloatingPointValues(const string array[], int n)
 
     int count = 0;
 
-    string plus = "+";    // Check to see if a + sign is in the string
-    string minus = "-";   // Checks to see if a - sign is in the string
-    string decimal = "."; // Checks to see if a optional decimal point
-
     for (int i = 0; i < n; i++)
     {
         string check = array[i];
-        if (strstr(check.c_str(), decimal.c_str()) && !strstr(check.c_str(), plus.c_str()) && !strstr(check.c_str(), minus.c_str()))
+        string plus = "+";
+        string minus = "-";
+
+        // use strstr to check if the string contains a + or -
+        if (validInteger(array[i]) && !strstr(check.c_str(), plus.c_str()) && !strstr(check.c_str(), minus.c_str()))
         {
             count++;
         }
-    }
-
-    string copy_array[n];
-    for (int i = 0; i < n; i++)
-    {
-        copy_array[i] = array[i];
+        else if (validFloat(array[i]) && !strstr(check.c_str(), plus.c_str()) && !strstr(check.c_str(), minus.c_str()))
+        {
+            count++;
+        }
     }
 
     return count;
@@ -163,9 +224,10 @@ int shiftLeft(string array[], int n, int amount, string placeholder)
         count++;
     }
 
+    // If I want to print the shifted array use this: Can remove if not necessary
     for (int i = 0; i < n; i++)
     {
-        cout << array[i] << " " << endl;
+        cout << array[i] << " ";
     }
 
     return count;
@@ -178,10 +240,10 @@ int main()
     string folks[8] = {"samwell", "jon", "margaery", "daenerys", "tyrion", "sansa", "magdalena", "jon"};
     string a[6] = {"123", "456", "789", "gamma", "beta", "delta"};
     string c[0] = {};
+    cout.setf(ios::boolalpha);
     /*
     cout << returnMax(test, 3) << endl;
     cout << locateMaximum(test, 3) << endl;
-    cout.setf(ios::boolalpha);
     cout << hasNoCapitals(test, 3) << endl;
     cout << hasNoCapitals(folks, 8) << endl;
     cout << countFloatingPointValues(data, 5) << endl;
@@ -194,4 +256,6 @@ int main()
     cout << shiftLeft(data, 5, 10, "foo");
     cout << shiftLeft(data, 5, 0, "foo") << endl;
     */
+    // cout << shiftLeft(data, 5, 1, "PERFECT") << endl;
+    // cout << countFloatingPointValues(data, 5) << endl;
 }
